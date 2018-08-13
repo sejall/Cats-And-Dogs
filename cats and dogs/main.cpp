71071e55822b95821b53398417a6196b22e211bc
sejall <33982474+sejall@users.noreply.github.com>
2018-08-13 07:26:34 +0530

    Add files via upload

---
+++ cats and dogs/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int c,d,l;
+        cin>>c>>d>>l;
+        if(l%4== || (c+d)*4<=l || l==4*c/2
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+    }
+    return 0;
+}
